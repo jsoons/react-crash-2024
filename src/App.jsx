e8f25ec70881c2851182d32a5191268991f99d3b
jsoons <jsoons@gmail.com>
2024-05-24 09:53:22 +0200

    some styling jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
 const App = () => {
+  const name = 'John'
+  const names = ['John', 'Mary', 'Louis']
+  const loggedIn = true
+  const styles = {
+    color: 'red',
+    fontSize: '55px'
+  }
+
   return (
-    <div>App</div>
+    <>
+      <div className='text-5xl'>App</div>
+      <p style={styles}>Hello {name} </p>
+      <ul>
+        {names.map((name,index)=> {
+          return <li key={index}>{name} welkom</li>
+}) }
+      </ul>
+      {loggedIn && <h1>Hello Member</h1>}
+    </>
   )
 }
 
