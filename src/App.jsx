ae94ec2a50de271cc9ad52739def8914bfb1c4a8
jsoons <jsoons@gmail.com>
2024-05-24 10:27:37 +0200

    clean for start project

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,8 @@
 import React from 'react'
 
 const App = () => {
-  const name = 'John'
-  const names = ['John', 'Mary', 'Louis']
-  const loggedIn = true
-  const styles = {
-    color: 'red',
-    fontSize: '55px'
-  }
-
   return (
-    <>
-      <div className='text-5xl'>App</div>
-      <p style={styles}>Hello {name} </p>
-      <ul>
-        {names.map((name,index)=> {
-          return <li key={index}>{name} welkom</li>
-}) }
-      </ul>
-      {loggedIn && <h1>Hello Member</h1>}
-    </>
+    <div>App</div>
   )
 }
 
