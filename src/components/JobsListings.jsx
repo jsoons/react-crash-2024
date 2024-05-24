dc4ee19d7a455727fdcf5cad7fab8e1e1a49a8fe
jsoons <jsoons@gmail.com>
2024-05-24 17:36:51 +0200

    recentJobs

--- src/components/JobsListings.jsx
+++ src/components/JobsListings.jsx
@@ -5,6 +5,7 @@ import JobListing from './JobListing'
 
 const JobsListings = () => {
     // console.log(jobs)
+    const recentJobs = jobs.slice(0,3)
     return (
         <section className="bg-blue-50 px-4 py-10">
             <div className="container-xl lg:container m-auto">
@@ -12,7 +13,7 @@ const JobsListings = () => {
                     Browse Jobs
                 </h2>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                    {jobs.map((job) => (
+                    {recentJobs.map((job) => (
                         <JobListing key={job.id} job={job}/>
 
                     ))}
