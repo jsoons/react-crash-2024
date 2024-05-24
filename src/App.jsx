835fae924c51442faade53f81f0c0400672fee2d
jsoons <jsoons@gmail.com>
2024-05-24 21:34:54 +0200

    setup react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+
 // import React from 'react'
 import Hero from "./components/Hero"
 import HomeCards from "./components/HomeCards"
@@ -5,22 +7,14 @@ import JobsListings from "./components/JobsListings"
 import NavBar from "./components/NavBar"
 import ViewAllJobs from "./components/ViewAllJobs"
 
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={<h1>My App</h1>}/>)
+)
 
 
 const App = () => {
-  return (
-    <>
-      {/* Content from  _theme_files/index.html */}
-      <NavBar/>
-      <Hero />
-      <HomeCards />
-      <JobsListings/>
-      <ViewAllJobs/>
-      
-      
-
-      
-    </>
+  return ( <RouterProvider router={router}/>
+    
   )
 }
 
