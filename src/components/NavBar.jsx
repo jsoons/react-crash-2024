e8d407f7d5027e877300ae5c3017cef74f1865c7
jsoons <jsoons@gmail.com>
2024-05-26 16:16:48 +0200

    NavLink ipv Link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 // import React from 'react'
 import logo from '../assets/images/logo.png'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
   return (
@@ -11,7 +11,7 @@ const NavBar = () => {
             className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
           >
             {/* <!-- Logo --> */}
-            <Link className="flex flex-shrink-0 items-center mr-4" to="/">
+            <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
               <img
                 className="h-10 w-auto"
                 src={logo}
@@ -20,21 +20,21 @@ const NavBar = () => {
               <span className="hidden md:block text-white text-2xl font-bold ml-2"
               >React Jobs</span
               >
-            </Link>
+            </NavLink>
             <div className="md:ml-auto">
               <div className="flex space-x-2">
-                <Link
+                <NavLink
                   to="/"
                   className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                >Home</Link>
-                <Link
+                >Home</NavLink>
+                <NavLink
                   to="/jobs"
                   className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                >Jobs</Link>
-                <Link
+                >Jobs</NavLink>
+                <NavLink
                   to="/add-job"
                   className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                >Add Job</Link>
+                >Add Job</NavLink>
               </div>
             </div>
           </div>
