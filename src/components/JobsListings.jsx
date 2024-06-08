2b9cee463cccfef592c567a7b704cb15c595a8cc
jsoons <jsoons@gmail.com>
2024-06-08 08:43:27 +0200

    spinner

--- src/components/JobsListings.jsx
+++ src/components/JobsListings.jsx
@@ -1,7 +1,8 @@
-import React , {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 // import jobs from '../jobs.json'
 import JobListing from './JobListing'
 // import { useState, useEffect } from 'react'
+import Spinner from './Spinner'
 
 const JobsListings = ({ isHome = false }) => {
     // console.log(jobs)
@@ -17,7 +18,7 @@ const JobsListings = ({ isHome = false }) => {
                 setJobs(data)
 
             } catch (error) {
-                console.log("Error fetching data",error)
+                console.log("Error fetching data", error)
             } finally {
                 setLoading(false)
             }
@@ -31,12 +32,20 @@ const JobsListings = ({ isHome = false }) => {
                 <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
                     {isHome ? 'Recent Jobs' : 'Browse Jobs'}
                 </h2>
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                    {jobs.map((job) => (
-                        <JobListing key={job.id} job={job} />
+                {loading ? (
+                    <Spinner loading={loading} />
+                ) : (<>
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
 
-                    ))}
-                </div>
+                        {jobs.map((job) => (
+                            <JobListing key={job.id} job={job} />
+
+                        ))}
+                    </div>
+
+
+                </>)}
+
             </div>
         </section>)
 }
