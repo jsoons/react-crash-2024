df42ce104b70892fd60849de9ddf073d91586c03
jsoons <jsoons@gmail.com>
2024-06-09 15:34:42 +0200

    cleanup of jobpage.jsx component

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,43 +1,18 @@
 import React from 'react'
-// import { useState, useEffect } from 'react'
 import { useParams, useLoaderData } from 'react-router-dom'
-import Spinner from '../components/Spinner'
-
-
 
 const JobPage = () => {
     const { id } = useParams()
     const job = useLoaderData()
-    // const [job, setJob] = useState(null)
-    // const [loading, setLoading] = useState(true)
-    // useEffect(() => {
-    //     const fetchJob = async () => {
-    //         try {
-    //             const res = await fetch(`/api/jobs/${id}`)
-    //             const data = await res.json()
-    //             console.log(data)
-    //             setJob(data)
-
-    //         } catch (error) {
-    //             console.log('Erro fetching data', error)
-    //         } finally {
-    //             setLoading(false)
-
-    //         }
-
-    //     }
-    //     fetchJob()
-    // }, [])
     return (
-        false ? <Spinner /> : <h1>{job.title}</h1>)
+        <h1>{job.title}</h1>
+    )
 }
 
-// const  JobLoader = async({params})
-
-const JobLoader = async ({params})=> {
+const JobLoader = async ({ params }) => {
     const res = await fetch(`/api/jobs/${params.id}`)
     const data = await res.json()
     return data
 }
 
-export {JobPage as default, JobLoader}
+export { JobPage as default, JobLoader }
