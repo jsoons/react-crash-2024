1da175ab82ccd48a770ee9cf5fd372936cc77782
jsoons <jsoons@gmail.com>
2024-06-09 15:14:09 +0200

    useParams hook en job id

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import Spinner from '../components/Spinner'
+
+
 
 const JobPage = () => {
-  return (
-    <div>JobPage</div>
-  )
+    const { id } = useParams()
+    const [job, setJob] = useState(null)
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        const fetchJob = async () => {
+            try {
+                const res = await fetch(`/api/jobs/${id}`)
+                const data = await res.json()
+                console.log(data)
+                setJob(data)
+
+            } catch (error) {
+                console.log('Erro fetching data', error)
+            } finally {
+                setLoading(false)
+
+            }
+
+        }
+        fetchJob()
+    }, [])
+    return (
+        loading ? <Spinner /> : <h1>{job.title}</h1>)
 }
+
+// const  JobLoader = async({params})
 
 export default JobPage
