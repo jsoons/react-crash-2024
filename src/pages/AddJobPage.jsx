07ecc8cb072b5104377673b485db746544b147dd
jsoons <jsoons@gmail.com>
2024-06-13 08:57:57 +0200

    newJob object added

--- src/pages/AddJobPage.jsx
+++ src/pages/AddJobPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import { PiColumnsPlusRightDuotone } from 'react-icons/pi'
 
 const AddJobPage = () => {
     const [title,setTitle] = useState('')
@@ -12,7 +13,27 @@ const AddJobPage = () => {
     const [contactEmail,setContactEmail] = useState('')
     const [contactPhone,setContactPhone] = useState('')
 
+    const submitForm = (e) => {
+      e.preventDefault()
 
+      const newJob = {
+        title,
+        type,
+        location,
+        description,
+        salary,
+        company: {
+          name: companyName,
+          description: companyDescription,
+          contactEmail,
+          contactPhone
+        }
+      }
+
+      console.log(newJob)
+    }
+
+    
 
   return (
     <>
@@ -21,7 +42,7 @@ const AddJobPage = () => {
         <div
           className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0"
         >
-          <form>
+          <form onSubmit={submitForm}>
             <h2 className="text-3xl text-center font-semibold mb-6">Add Job</h2>
 
             <div className="mb-4">
